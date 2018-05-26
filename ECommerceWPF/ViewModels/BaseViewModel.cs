423aec39b4196c28a699184bc49b3af7b19c566b
Veillault Adrien <veillault.adrien@gmail.com>
2018-05-26 10:49:01 +0200

    Add ViewModels, View and beginning of binding

--- ECommerceWPF/ViewModels/BaseViewModel.cs
+++ ECommerceWPF/ViewModels/BaseViewModel.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ECommerceWPF.ViewModels
 {
-    class BaseViewModel
+    /// <summary>
+    /// Classe de base pour tous les Vues-Modèles de l'application
+    /// Elle fournit la gestion du PropertyChanged via l'interface INotifyPropertyChanged
+    /// Classe abstraite
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
+
+        #region Constructeurs
+        protected BaseViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Membres de INotifyPropertyChanged
+
+        /// <summary>
+        /// Se déclenche lorsqu'une propriété de cet objet a une nouvelle valeur
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Déclenche l'évènement sur  l'évènement PropertyChanged
+        /// </summary>
+        /// <param name="propertyName">La propriété qui a une nouvelle valeur</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        #endregion
+
     }
 }
---
+++ ECommerceWPF/ViewModels/DetailProduitViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ECommerceWPF.ViewModels
+{
+    public class DetailProduitViewModel : BaseViewModel
+    {
+        #region Variables
+
+        private string _code;
+        private string _nom;
+        private RelayCommand _addOperation;
+
+        #endregion
+
+        #region Constructeurs
+
+        /// <summary>
+        /// Constructeur par défaut
+        /// <param name="p">Produit à transformer en DetailProduitViewModel</param>
+        /// </summary>
+        public DetailProduitViewModel(Produit p) // à adapater à la BD
+        {
+            _code = p.Code;
+            _nom = p.Nom;
+        }
+
+        #endregion
+
+        #region Data Bindings
+
+        /// <summary>
+        /// Code du produit
+        /// </summary>
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        /// <summary>
+        /// Nom du produit
+        /// </summary>
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        #endregion
+
+        #region Commandes
+
+        /// <summary>
+        /// Commande pour ouvrir la fenêtre pour ajouter une opération
+        /// </summary>
+        public ICommand AddOperation
+        {
+            get
+            {
+                if (_addOperation == null)
+                    _addOperation = new RelayCommand(() => this.ShowWindowOperation());
+                return _addOperation;
+            }
+        }
+
+        /// <summary>
+        /// Permet l'ouverture de la fenêtre
+        /// </summary>
+        private void ShowWindowOperation()
+        {
+            Views.Operation operationWindow = new Views.Operation();
+            operationWindow.DataContext = this;
+            operationWindow.ShowDialog();
+        }
+
+        #endregion
+    }
+}
+}
