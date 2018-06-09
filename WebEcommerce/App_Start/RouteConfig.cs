bda2da965e42673a1c1aa57fb3c30ff30261bec2
Veillault Adrien <veillault.adrien@gmail.com>
2018-06-09 11:56:43 +0200

    Add Views and Controller

---
+++ WebEcommerce/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebEcommerce
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "EditProduit",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Produit", action = "EditProduit", id = UrlParameter.Optional },
+                constraints: new { id = @"\d+" }
+            );
+
+
+            routes.MapRoute(
+                name: "DetailsProduit",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Produit", action = "GetProduit", id = UrlParameter.Optional },
+                constraints: new {id=@"\d+"}
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebEcommerce/Controllers/ProduitController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.e_commerce;
+using Modele.e_commerce.Modele.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEcommerce.Controllers
+{
+    public class ProduitController : Controller
+    {
+        // GET: Produit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListProduit()
+        {
+            List<Produit> p = BusinessManager.Instance.GetAllProduit();
+            return View("ListProduit",p);
+        }
+
+        public ActionResult GetProduit(int id)
+        {
+            Produit p = BusinessManager.Instance.GetProduit(id);
+            return View("DetailsProduit", p);
+        }
+
+        public ActionResult EditProduit(int id)
+        {
+            Produit p = BusinessManager.Instance.GetProduit(id);
+            return View("EditProduit", p);
+        }
+    }
+}
