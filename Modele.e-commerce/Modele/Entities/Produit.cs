026415076c3448d5cda4fe132e1e03a783c33941
Veillault Adrien <veillault.adrien@gmail.com>
2018-06-16 09:45:48 +0200

    Form with HtmlHelper

--- Modele.e-commerce/Modele/Entities/Produit.cs
+++ Modele.e-commerce/Modele/Entities/Produit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,19 @@ namespace Modele.e_commerce.Modele.Entities
     {
         public int IDProduit { get; set; }
         public int Code { get; set; }
+
+        [Display(Name = "Libellé")]
         public string Libelle { get; set; }
+        [Display(Name = "Description")]
         public string Description { get; set; }
+        [Display(Name = "Actif")]
         public Boolean Actif { get; set; }
+        [Display(Name = "Stock")]
         public int Stock { get; set; }
+        [Display(Name = "Prix")]
         public float Prix { get; set; }
         public int CategorieId { get; set; }
+        [Display(Name = "Categorie")]
         public Categorie Categorie { get; set; }
     }
 }
--- WebEcommerce/Controllers/ProduitController.cs
+++ WebEcommerce/Controllers/ProduitController.cs
@@ -16,22 +16,35 @@ namespace WebEcommerce.Controllers
             return View();
         }
 
+        [ActionName("list")]
         public ActionResult ListProduit()
         {
             List<Produit> p = BusinessManager.Instance.GetAllProduit();
             return View("ListProduit",p);
         }
 
+        [ActionName("get")]
         public ActionResult GetProduit(int id)
         {
             Produit p = BusinessManager.Instance.GetProduit(id);
             return View("DetailsProduit", p);
         }
 
+        [ActionName("edit")]
         public ActionResult EditProduit(int id)
         {
             Produit p = BusinessManager.Instance.GetProduit(id);
             return View("EditProduit", p);
+        }
+
+        [HttpPost][ActionName("editionProduit")]
+        public ActionResult ChangeProduit(Produit model)
+        {
+            if(ModelState.IsValid)
+            {
+                BusinessLayer.e_commerce.BusinessManager.Instance.ModifierProduit(model);
+            }
+            return View("DetailsProduit", model);
         }
     }
 }
